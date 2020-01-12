1ab0dda696785d0536b478a9b51a5653513914a1
Shoe2 <andrewelee1983@gmail.com>
2020-01-11 19:49:44 -0800

    changes

---
+++ server/server.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8082;
+const mongoose = require('mongoose');
+const config = require('environment');
+const uri = config.get('mongoURI');
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+
+
+
+
