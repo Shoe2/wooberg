cbeb1331456cbf935d12f4bc6e9c22fac515388c
Shoe2 <andrewelee1983@gmail.com>
2020-02-18 13:59:16 -0800

    react skeleton

---
+++ client/wooberg/src/components/add-format.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AddFormatComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Todos List Component!!</p>
+            </div>
+        )
+    }
+}
