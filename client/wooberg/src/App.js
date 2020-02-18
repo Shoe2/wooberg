9ecfae72bdfad26f0f08dbb318f342920cd4afdb
Shoe2 <andrewelee1983@gmail.com>
2020-02-18 13:47:27 -0800

    setting up react

--- client/wooberg/src/App.js
+++ client/wooberg/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import FormatComponent from "components/format.component";
+import BlogComponent from "components/blog.component";
+import AddFormatComponent from "components/add-format.component";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-test
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          test
       </header>
-    </div>
+      </div>
+      <Route path="/" exact component={BlogComponent} />
+      <Route path="/format/:id" component={FormatComponent} />
+      <Route path="/add-format" component={AddFormatComponent} />
+    </Router>
+
   );
 }
 
