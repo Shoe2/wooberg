ada5efbdc2de5a4890ba6e030634d1e331e6554c
Shoe2 <andrewelee1983@gmail.com>
2020-08-24 16:08:48 -0700

    render html from db

--- client/src/components/format.component.js
+++ client/src/components/format.component.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import renderHTML from 'react-render-html';
 
 export default class FormatComponent extends Component {
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
             formatId: props.match.params.id
         };
@@ -15,16 +16,28 @@ export default class FormatComponent extends Component {
             .then(res => res.text()
                 .then((data) => {
                     this.setState({ format: JSON.parse(data) });
-                }, (error)=>{
+                }, (error) => {
                     console.log(error);
                 }))
             .catch(console.log);
+
+
+
     }
 
     render() {
+
         return (
             <div>
-                <p>{this.state.format?.title}</p>
+                <h5>{this.state.format?.title}</h5>
+        <div id="formatRules">{this.state.format ? renderHTML(this.state.format?.Rules) : ""}</div>
+                <h6>Banned List</h6>
+                <p>{this.state.format?.BannedList}</p>
+                <h6>RestrictedList</h6>
+                <p>{this.state.format?.RestrictedList}</p>
+                <h6>History</h6>
+                <p>this format was originally created by{this.state.format?.creator}</p>
+                <p>{this.state.format?.history}</p>
             </div>
         )
     }
